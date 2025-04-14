01294d973b74d45b631743fd168d06691c7e488e
N0ViP <01.xx.vip@gmail.com>
2025-04-14 20:44:38 +0100

    start coding

--- ft_exit.c
+++ ft_exit.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/14 08:29:06 by yjaafar           #+#    #+#             */
-/*   Updated: 2025/04/14 10:35:35 by yjaafar          ###   ########.fr       */
+/*   Created: 2025/04/14 09:48:44 by yjaafar           #+#    #+#             */
+/*   Updated: 2025/04/14 10:37:19 by yjaafar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <unistd.h>
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef	struct s_stuff
+void	ft_exit(char err)
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat;
-}	t_stuff;
-
-int		ft_atoi(char *s);
-void	ft_exit(char err);
-
-#endif
+	if (err == 2)
+		printf("Usage: ./philo <number_of_philosophers> <time_to_die> <time_to_eat>\
+	<time_to_sleep> <number_of_times_each_philosopher_must_eat(optinal)>");
+	else if (err == 1)
+		printf("Invalid argument\n");
+	exit(err);
+}
