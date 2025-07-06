cb0890826b3b15491dc315c487603d06c026bd76
N0ViP <01.xx.vip@gmail.com>
2025-07-06 11:25:42 +0100

    working on start

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -1,5 +1,14 @@
 #include "philo_bonus.h"
 
+void	clean_up(t_stuff *stuff)
+{
+	sem_close(stuff->lock);
+	sem_close(stuff->forks);
+	sem_unlink("/forks");
+	sem_unlink("/lock");
+	free(stuff->philos);
+}
+
 void one_philo(int t_to_die)
 {
 	pid_t	pid;
@@ -23,6 +32,19 @@ void one_philo(int t_to_die)
 	exit(EXIT_SUCCESS);
 }
 
+void	kill_philos(t_stuff *stuff, int n_of_philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < n_of_philos)
+	{
+		kill(stuff->philos[i], SIGKILL);
+		i++;
+	}
+	clean_up(stuff);
+}
+
 void	allocate_philos_forks(t_stuff *stuff)
 {
 	stuff->p_pid = getpid();
@@ -37,11 +59,7 @@ void	allocate_philos_forks(t_stuff *stuff)
 	stuff->forks = sem_open("/forks", O_CREAT, 0777, stuff->number_of_philos);
 	if (!stuff->forks || !stuff->lock)
 	{
-		sem_close(stuff->lock);
-		sem_close(stuff->forks);
-		sem_unlink("/forks");
-		sem_unlink("/lock");
-		free(stuff->philos);
+		clean_up(stuff);
 		exit(EXIT_FAILURE);
 	}
 	sem_wait(stuff->lock);
@@ -71,14 +89,109 @@ void	run_philos(t_stuff *stuff)
 		}
 		i++;
 	}
+	i = 0;
+	if (getpid() == stuff->p_pid)
+	{
+		while (i < stuff->philos)
+		{
+			sem_post(stuff->lock);
+			i++;
+		}
+	}
+}
+
+void	philos_died(t_stuff *stuff, int pid)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	kill_philos(stuff, stuff->number_of_philos);
+	printf("%lld\t%d\tdied\n", time_ms(&tv) - \
+		time_ms(&stuff->tv_start), pid);
+	exit(EXIT_FAILURE);
+}
+
+void	wait_child(t_stuff *stuff)
+{
+	int	exit_child_val;
+	int	i;
+
+	i = 1;
+	while (true)
+	{
+		waitpid(-1, &exit_child_val, 0);
+		if (exit_child_val != 0)
+		{
+			philo_died(stuff, exit_child_val);
+		}
+		if (stuff->must_eat != 0 && i == stuff->number_of_philos)
+		{
+			kill_philos(stuff, stuff->number_of_philos);
+			exit(EXIT_SUCCESS);
+		}
+		i++;
+	}
+}
+
+void	start(void *arg)
+{
+	t_stuff	*stuff;
+
+	stuff = (t_stuff *) arg;
+	sem_wait(stuff->lock);
+	pthread_mutex_lock(&stuff->time_protection);
+	gettimeofday(&stuff->tv_beg, NULL);
+	pthread_mutex_unlock(&stuff->time_protection);
+	while ()
+	{
+		thinking(stuff);
+		eating(stuff);
+		sleeping(stuff);
+	}
+}
+
+void	monitor(t_stuff *stuff)
+{
+	;
+}
+
+void init_mutexs(t_stuff *stuff)
+{
+	if (pthread_mutex_init(&stuff->alive_protection, NULL))
+	{
+		clean_up(stuff);
+		exit(EXIT_FAILURE);
+	}
+	if (pthread_mutex_init(&stuff->eat_protection, NULL))
+	{
+		pthread_mutex_destroy(&stuff->alive_protection);
+		clean_up(stuff);
+		exit(EXIT_FAILURE);
+	}
+	if (pthread_mutex_init(&stuff->time_protection, NULL))
+	{
+		pthread_mutex_destroy(&stuff->alive_protection);
+		pthread_mutex_destroy(&stuff->eat_protection);
+		clean_up(stuff);
+		exit(EXIT_FAILURE);
+	}
 }
 
 void	run_simulation(t_stuff *stuff)
 {
-	
+	 pthread_t	*monitor;
+	 pthread_t	*philo;
+
+	gettimeofday(&stuff->tv_start, NULL);
+	gettimeofday(&stuff->tv_beg, NULL);
+	init_mutexs(stuff);
+	 if (pthread_create(philo, NULL, start, stuff))
+	 	exit(EXIT_FAILURE);
+	if (pthread_create(philo, NULL, monitor, stuff))
+		exit(EXIT_FAILURE);
 }
 
-bool	init_philos(t_stuff *stuff)
+void	init_philos(t_stuff *stuff)
 {
 	allocate_philos_forks(stuff);
 	init_time(stuff);
@@ -89,31 +202,36 @@ bool	init_philos(t_stuff *stuff)
 		wait_child(stuff);
 }
 
+void	init_stuff(t_stuff *stuff, int ac, char *av[])
+{
+	if (ac != 5 && ac != 6)
+		return (write(2, "Invalid arguments!\n", 19), 1);
+	stuff->al
+	stuff->must_eat = 0;
+	stuff->number_of_philos = ft_atoi(av[1]);
+	stuff->t_to_die = ft_atoi(av[2]);
+	stuff->t_to_eat = ft_atoi(av[3]);
+	stuff->t_to_sleep = ft_atoi(av[4]);
+	if (stuff->number_of_philos <= 0
+		|| stuff->t_to_die <= 0
+		|| stuff->t_to_eat <= 0
+		|| stuff->t_to_sleep <= 0)
+		return (write(2, "Invalid arguments!\n", 19), 1);
+	if (ac == 6)
+	{
+		stuff->must_eat = ft_atoi(av[5]);
+		if (stuff->must_eat <= 0)
+			return (1);
+	}
+}
+
 int main(int ac, char **av)
 {
     t_stuff	stuff;
 	int		reval;
-
-	if (ac != 5 && ac != 6)
-		return (write(2, "Invalid arguments!\n", 19), 1);
-	stuff.must_eat = 0;
-	stuff.number_of_philos = ft_atoi(av[1]);
-	stuff.t_to_die = ft_atoi(av[2]);
-	stuff.t_to_eat = ft_atoi(av[3]);
-	stuff.t_to_sleep = ft_atoi(av[4]);
-	if (stuff.number_of_philos <= 0
-		|| stuff.t_to_die <= 0
-		|| stuff.t_to_eat <= 0
-		|| stuff.t_to_sleep <= 0)
-		return (write(2, "Invalid arguments!\n", 19), 1);
-	if (ac == 6)
-	{
-		stuff.must_eat = ft_atoi(av[5]);
-		if (stuff.must_eat <= 0)
-			return (1);
-	}
+	init_stuff(&stuff, ac, av);
 	if (stuff.number_of_philos == 1)
 		one_philo(stuff.t_to_die);
-    reval = init_philos(&stuff);
-	return (!reval);
+    init_philos(&stuff);
+	return (0);
 }
--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -1,37 +1,35 @@
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
 
+# include <fcntl.h>
+# include <stdio.h>
+# include <signal.h>
 # include <unistd.h>
+# include <stdlib.h>
 # include <pthread.h>
 # include <stdbool.h>
-# include <stdio.h>
-# include <stdlib.h>
 # include <sys/time.h>
-# include <semaphore.h>
-# include <fcntl.h>
 # include <sys/wait.h>
+# include <semaphore.h>
 
 typedef struct s_stuff
 {
 	struct timeval	tv_start;
+	struct timeval	tv_beg;
+	pthread_mutex_t	alive_protection;
+	pthread_mutex_t	eat_protection;
+	pthread_mutex_t	time_protection;
 	sem_t			*forks;
 	sem_t			*lock;
 	pid_t			*philos;
 	pid_t			philo_id;
 	pid_t			p_pid;
+	bool			alive;
 	int				number_of_philos;
 	int				t_to_die;
 	int				t_to_eat;
 	int				t_to_sleep;
 	int				must_eat;
 } t_stuff;
-
-typedef struct t_philo
-{
-	struct timeval	tv_beg;
-	t_stuff			*stuff;
-	int				eat;
-	bool			alive;
-}	t_philo;
 
 #endif
